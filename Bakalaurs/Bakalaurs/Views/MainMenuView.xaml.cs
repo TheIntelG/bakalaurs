678b45040171a42e12568d9040055490238b9335
Rihards <rihards.krastins14@gmail.com>
2021-05-02 19:33:35 +0300

    Added File Upload

--- Bakalaurs/Bakalaurs/Views/MainMenuView.xaml.cs
+++ Bakalaurs/Bakalaurs/Views/MainMenuView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace Bakalaurs.Views
@@ -11,13 +13,42 @@ namespace Bakalaurs.Views
         public MainMenuView()
         {
             InitializeComponent();
-
-            this.DataContext = this;
         }
 
         private void UploadButton_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
 
+            bool? response = openFileDialog.ShowDialog();
+
+            if(response == true)
+            {
+                string filePath = openFileDialog.FileName;
+
+                FileNameLabel.Content = openFileDialog.SafeFileName;
+
+                MessageBox.Show(filePath);
+            }
+        }
+
+        private void FileDropStackPanel_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                if (files.Length == 1)
+                {
+                    string filename = Path.GetFileName(files[0]);
+
+                    FileNameLabel.Content = filename;
+
+                    MessageBox.Show(Path.GetFullPath(files[0]));
+                } else
+                {
+                    MessageBox.Show("Upload only 1 design at a time!");
+                }
+            }
         }
     }
 }
