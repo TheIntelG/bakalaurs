02da582f54ca506d5c4e065e89657a7c985e3926
Rihards <rihards.krastins14@gmail.com>
2021-05-02 18:13:52 +0300

    Pre-Upload button

--- Bakalaurs/Bakalaurs/ViewModels/MainMenuViewModel.cs
+++ Bakalaurs/Bakalaurs/ViewModels/MainMenuViewModel.cs
@@ -1,16 +1,19 @@
-﻿using Bakalaurs.Stores;
+﻿using Bakalaurs.Commands;
+using Bakalaurs.Stores;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Bakalaurs.ViewModels
 {
     class MainMenuViewModel : ViewModelBase
     {
-
+        public ICommand QuitCommand { get; }
         public MainMenuViewModel(NavigationStore navigationStore)
         {
-
+            QuitCommand = new RelayCommand((par) => { Application.Current.Shutdown(); });
         }
     }
 }
--- Bakalaurs/Bakalaurs/Views/MainMenuView.xaml.cs
+++ Bakalaurs/Bakalaurs/Views/MainMenuView.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Bakalaurs.Views
 {
@@ -21,6 +11,13 @@ namespace Bakalaurs.Views
         public MainMenuView()
         {
             InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
