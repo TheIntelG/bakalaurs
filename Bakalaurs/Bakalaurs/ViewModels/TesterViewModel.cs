daa3a2ec77f52eaa765875db38f85dc3a5f6077e
Rihards <rihards.krastins14@gmail.com>
2021-05-06 15:00:23 +0300

    Added Image Loading

--- Bakalaurs/Bakalaurs/ViewModels/TesterViewModel.cs
+++ Bakalaurs/Bakalaurs/ViewModels/TesterViewModel.cs
@@ -1,18 +1,20 @@
 ﻿using Bakalaurs.Commands;
 using Bakalaurs.Stores;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Windows.Input;
 
 namespace Bakalaurs.ViewModels
 {
     public class TesterViewModel : ViewModelBase
     {
+
         public ICommand NavigateElementMenuCommand { get; set; }
+        public string CanvasImage { get; }
         public TesterViewModel(NavigationStore navigationStore)
         {
             NavigateElementMenuCommand = new NavigateCommand<ElementMenuViewModel>(navigationStore, () => new ElementMenuViewModel(navigationStore));
+
+            DataLibrary dataLibrary = new DataLibrary();
+            CanvasImage = dataLibrary.getDesignFilePath();
         }
     }
 }
