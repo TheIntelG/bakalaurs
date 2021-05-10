19cafe00f943562e3618e3b8c8088628246a73df
Rihards <rihards.krastins14@gmail.com>
2021-05-10 17:47:19 +0300

    don't remember what i did here xD

--- Bakalaurs/Bakalaurs/obj/Release/netcoreapp3.1/Views/TesterView.g.cs
+++ Bakalaurs/Bakalaurs/obj/Release/netcoreapp3.1/Views/TesterView.g.cs
@@ -69,7 +69,7 @@ namespace Bakalaurs.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Bakalaurs;V1.0.0.0;component/views/testerview.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Bakalaurs;component/views/testerview.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\TesterView.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
